8a01edb06e6484d0f1efca3848998f63ba817e4a
AdrianoBispo <Drianvini10@gmail.com>
2022-07-25 17:40:29 -0300

    No arquivo html removi os comandos scripts do head e coloquei no body. No arquivo .js adicionei os códigos da bolinha e da raquete, onde a raquete se movimenta ao clicarmos a UP_ARROW e DOWN_ARROW. Também foi adicionado o movimento da bolinha, no caso dela tocar na raquete ou nas bordas.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,69 @@
+// Posição da Bolinha (x,y)
+let ball_X = 300;
+let ball_Y = 200;
+
+// Tamanho da Bolinha
+let diametro = 20;
+let raio = diametro / 2;
+
+// Velocidade da Bolinha
+let velocity_X = 3; 
+let velocity_Y = 3;
+
+// Posição da Raquete (x,y)
+let racket_X = 25
+let racket_Y = 170
+
+// Tamanho da Raquete (Width x Height)
+let racket_Width = 15
+let racket_Height = 70
+  
 function setup() {
-  // put setup code here
+  createCanvas(600, 400);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  Ball();
+  movimentBall();
+  collisionBall();
+  Racket();
+  movimentRacket();
+  collisionRacket();
+}
+
+function Ball() {
+  circle(ball_X, ball_Y, diametro);
+}
+
+function movimentBall() {
+  ball_X += velocity_X;
+  ball_Y += velocity_Y;
+}
+
+function collisionBall() {
+  if (ball_X + raio> width || ball_X - raio < 0)
+    velocity_X *= -1
+  
+  if (ball_Y + raio > height || ball_Y - raio < 0)
+    velocity_Y *= -1
+}
+
+function Racket() {
+  rect(racket_X, racket_Y, racket_Width, racket_Height)
+}
+
+function movimentRacket() {
+  if(keyIsDown(UP_ARROW)){
+    racket_Y -= 10;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    racket_Y += 10;
+  }
+}
+
+function collisionRacket() {
+  if(ball_X - raio < racket_X + racket_Width && ball_Y - raio < racket_Y + racket_Height && ball_Y + raio > racket_Y) {
+    velocity_X *= -1;
+  }
 }
